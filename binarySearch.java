a2d8f1845c102e864a0be272da6dc382bfe58f27
ptrish12 <pstichnoth@gmail.com>
2020-07-08 20:03:33 -0500

    updated binary search to take user input

--- binarySearch.java
+++ binarySearch.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class binarySearch {
 
     private static int doBinarySearch (int[] listToSearch, int key) {
+        // implements binary search from an input array and an int key.
         int high = listToSearch.length - 1;
         int low = 0;
         int mid = (high - low) / 2;
@@ -12,7 +14,7 @@ public class binarySearch {
             if (key == listToSearch[mid]) {
                 return mid;
             } else if (key < listToSearch[mid]) {
-                high = mid;
+                high = mid-1;
                 mid = (high - low) / 2;
             } else if (key > listToSearch[mid]) {
                 low = mid;
@@ -20,28 +22,69 @@ public class binarySearch {
             }
         }
         return -1;
-
     }
 
-    
+    private static int getSearchKey () {
+        // User inputs number to search for
+        System.out.println("Enter an integer to search for.");
+        Scanner getKey = new Scanner(System.in);
+        int searchKey = getKey.nextInt();
+        return searchKey;
+    }
+
+    private static int[] getUserListToSearch() {
+        // get user-provided array to search in
+        System.out.println("How long is your array?");
+        Scanner getN = new Scanner(System.in);
+        int n = getN.nextInt();
+
+        int[] listToSearch = new int[n];
+        Scanner getList = new Scanner(System.in);
+        System.out.println("Enter a series of numbers.");
+
+        for (int i = 0; i < n; i++) {
+            listToSearch[i] = getList.nextInt();
+        }
+        Arrays.sort(listToSearch);
+        return listToSearch;
+    }
+
+    private static int[] getRandomList() {
+        // let computer provide random array to search.
+        System.out.println("How long is the array?");
+        Scanner getN = new Scanner(System.in);
+        int n = getN.nextInt();
+
+        int[] listToSearch = new int[n];
+
+        Random rand = new Random();
+        for (int i = 0; i < listToSearch.length; i ++) {
+            listToSearch[i] = rand.nextInt(50);
+        }
+        Arrays.sort(listToSearch);
+        return listToSearch;
+    }
 
     public static void main (String[] args) {
-        int[] searchList = {0, 5, 9, 9, 13, 16, 20, 22, 23, 24, 27, 34, 37, 43, 45};
-//        Random rand = new Random();
-//        for (int i = 0; i < searchList.length; i ++) {
-//            searchList[i] = rand.nextInt(50);
-//        }
-//        Arrays.sort(searchList);
+
+        System.out.println("Do you want the computer to provide a random array? Enter y or n.");
+        Scanner inputEntry = new Scanner(System.in);
+        String inputType = inputEntry.nextLine();
+
+        int[] searchList;
+
+        if (inputType.equals("y")) {
+            searchList = getRandomList();
+        } else if (inputType.equals("n")){
+            searchList = getUserListToSearch();
+        } else {
+            searchList = getRandomList();
+        }
+
+        int keyNumber = getSearchKey();
         System.out.println(Arrays.toString(searchList));
-        int result = doBinarySearch(searchList, 0);
+        int result = doBinarySearch(searchList, keyNumber);
         System.out.println(result);
-        result = doBinarySearch(searchList, 5);
-        System.out.println(result);
-        result = doBinarySearch(searchList, 20);
-        System.out.println(result);
-        result = doBinarySearch(searchList, 22);
-        System.out.println(result);
-        result = doBinarySearch(searchList, 45);
-        System.out.println(result);
+
     }
 }
